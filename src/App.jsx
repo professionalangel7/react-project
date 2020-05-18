a84f844fa248559e918690fde533658cd188000c
Amber <professionalangel7@gmail.com>
2020-05-18 02:23:19 -0700

    React-Project

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import News from './components/News';
+import Footer from './components/Footer';
+import Contact from './components/Contact';
+
+import Navbar from './components/CustomNavbar';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Navbar />
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/news" component={News} />
+          <Route path="/contact" component={Contact} />          
+          <Footer /> 
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+import { Jumbotron, Grid, Row, Col, Image, Button } from 'react-bootstrap';
+import './Home.css';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <Grid>
+        <Jumbotron>
+          <h2>915Help</h2>
+          <p>ful Hints for Real Estate Agents </p>
+          <Link to="/about">
+            <Button bsStyle="primary">Learn More</Button>
+          </Link>
+        </Jumbotron>
+        <Row className="show-grid text-center">
+          <Col xs={12} sm={4} className="person-wrapper">
+            <Image src="assets/sherri.jpg" circle className="profile-pic"/>
+            <h3>Sherri</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+          </Col>
+          <Col xs={12} sm={4} className="person-wrapper">
+            <Image src="assets/sylvia.jpg" circle className="profile-pic"/>
+            <h3>Sylvia</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          </Col>
+          <Col xs={12} sm={4} className="person-wrapper">
+            <Image src="assets/tina.jpg" circle className="profile-pic"/>
+            <h3>Tina</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+          </Col>
+        </Row>
+      </Grid>
+    )
+  }
+}
